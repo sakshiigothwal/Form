74d4bd0c60376d833901d0baddfb8e8da6f10dc9
sakshiigothwal <119726876+sakshiigothwal@users.noreply.github.com>
2025-06-02 17:13:51 +0530

    Update index.js

--- Form/index.js
+++ Form/index.js
@@ -1,4 +1,5 @@
 const form = document.getElementById("registrationForm");
+const errMsg = document.getElementById("error");
 
   const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   const passwordPattern = /^.{8,}$/; 
@@ -10,23 +11,33 @@ form.addEventListener("submit", function (e) {
   const email = document.getElementById("email").value.trim();
   const password = document.getElementById("password").value.trim();
 
-  if (!emailPattern.test(email) || !passwordPattern.test(password)) {
-    return; 
+  errMsg.innerText="";
+
+  if (!emailPattern.test(email)) {
+    errMsg.innerText = "Please enter a valid email address.";
+    return;
   }
 
+  if (!passwordPattern.test(password)) {
+    errMsg.innerText = "Password must be at least 8 characters long.";
+    return;
+  }
+
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+
+  const alreadyExists = users.some(u => u.email === email);
+  if (alreadyExists){
+    errMsg.innerText = "An account with this email already exists.";
+    return;
+  }
   const user = {
     name,
     email,
     password
   };
-
-  let users = JSON.parse(localStorage.getItem("users")) || [];
-
-  const alreadyExists = users.some(u => u.email === email);
-  if (alreadyExists) return;
-
   users.push(user);
   localStorage.setItem("users", JSON.stringify(users));
 
   form.reset();
+  errMsg.innerText = "Registration successful!";
 });
